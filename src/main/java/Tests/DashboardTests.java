524a2a417816a85ecdfad4fa1def891cc00435bf
mnaeem <mnaeem@gmail.com>
2023-04-08 16:08:56 +0500

    Tests are grouped in Tests package TestNG XML file is added

---
+++ src/main/java/Tests/DashboardTests.java
@@ -0,0 +1,59 @@
+package Tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class DashboardTests {
+
+    public WebDriver driver;
+    public String demoURL = "https://demo.guru99.com/test/newtours/";
+
+    @BeforeTest
+    public void setup(){
+    driver = new ChromeDriver();
+    driver.get(demoURL);
+    }
+
+    @Test(priority = 1, groups = "Smoke Test")
+    public void validateMercuryPageTitle(){
+
+        String actualMercuryPageTile = driver.getTitle();
+        String expectedMercuryPageTitle = "Welcome: Mercury Tours";
+        //Validate Page Title
+         Assert.assertEquals(actualMercuryPageTile,expectedMercuryPageTitle);
+
+    }
+
+    @Test(priority = 2, groups="Regression Tests")
+    public void validateLoginTest(){
+      //  driver.get(demoURL);
+        WebElement userNameTextField = driver.findElement(By.name("userName"));
+        userNameTextField.sendKeys("test");
+
+        WebElement passwordTextField = driver.findElement(By.name("password"));
+        passwordTextField.sendKeys("test");
+        WebElement submitButton = driver.findElement(By.name("submit"));
+        submitButton.click();
+
+        //Dashboard
+        WebElement successMessage = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3"));
+        String successMessageText = successMessage.getText();
+        String expectedMessageText = "Login Successfully";
+        Assert.assertEquals(successMessageText,expectedMessageText);
+    }
+
+
+
+    @AfterTest
+    public void Exit(){
+        driver.quit();
+    }
+
+}
--- src/main/java/Tests/RegistrationTests.java
+++ src/main/java/Tests/RegistrationTests.java
@@ -1,61 +1,30 @@
-import net.bytebuddy.build.Plugin;
+package Tests;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.apache.commons.lang3.RandomStringUtils;
 
-import java.util.Random;
-
-public class TestNGTests {
+public class RegistrationTests {
 
     public WebDriver driver;
     public String demoURL = "https://demo.guru99.com/test/newtours/";
 
     @BeforeTest
     public void setup(){
-  //  ChromeOptions co = new ChromeOptions();
-   // co.addArguments("--remote-allow-origins=*");
-    //Launch browser
-    driver = new ChromeDriver();
-    driver.get(demoURL);
+        //  ChromeOptions co = new ChromeOptions();
+        // co.addArguments("--remote-allow-origins=*");
+        //Launch browser
+        driver = new ChromeDriver();
+        driver.get(demoURL);
     }
-
-    @Test(priority = 1)
-    public void validateMercuryPageTitle(){
-        //Navigate url
-    //    driver.get(demoURL);
-        String actualMercuryPageTile = driver.getTitle();
-        String expectedMercuryPageTitle = "Welcome: Mercury Tours";
-        //Validate Page Title
-        Assert.assertEquals(actualMercuryPageTile,expectedMercuryPageTitle);
-    }
-
-    @Test(priority = 2)
-    public void validateLoginTest(){
-      //  driver.get(demoURL);
-        WebElement userNameTextField = driver.findElement(By.name("userName"));
-        userNameTextField.sendKeys("test");
-        WebElement passwordTextField = driver.findElement(By.name("password"));
-        passwordTextField.sendKeys("test");
-        WebElement submitButton = driver.findElement(By.name("submit"));
-        submitButton.click();
-
-        //Dashboard
-        WebElement successMessage = driver.findElement(By.xpath("/html/body/div[2]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[1]/td/h3"));
-        String successMessageText = successMessage.getText();
-        String expectedMessageText = "Login Successfully";
-        Assert.assertEquals(successMessageText,expectedMessageText);
-    }
-
-    @Test(priority = 3, groups = "Functional", description = "To verify registration of new user")
+    @Test(groups = "Functional", description = "To verify registration of new user")
     public void validateRegister(){
 
         // Test Data
