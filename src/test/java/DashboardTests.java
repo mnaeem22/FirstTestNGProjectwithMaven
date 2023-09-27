ea71011ffcf169d6f22287c303b0c9f89b0de949
mnaeem <mnaeem@gmail.com>
2023-09-27 15:06:39 +0500

    Tests are grouped in Tests package TestNG XML file is added

--- src/test/java/DashboardTests.java
+++ src/test/java/DashboardTests.java
@@ -1,15 +1,11 @@
-package Tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.apache.commons.lang3.RandomStringUtils;
 
 public class DashboardTests {
 
--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,10 +1,7 @@
-package Tests;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
